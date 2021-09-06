7fb71f55d96dc28667b0fd31aca7d701d22f0f74
Thea U. Kjeldsmark <49352969+theauk@users.noreply.github.com>
2021-09-06 17:49:14 +0200

    Created user interface for IsLeapYear (#4)
    * Tested and implemented IsLeapYear
    
    * Created user interface for IsLeapYear

--- LeapYear.Tests/ProgramTests.cs
+++ LeapYear.Tests/ProgramTests.cs
@@ -1,10 +1,12 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace LeapYear.Tests
 {
     public class ProgramTests
     {
+        
         [Fact]
         public void Divisible_by_Four()
         {
@@ -27,6 +29,36 @@ namespace LeapYear.Tests
         public void Not_Centurial_Leap_Year()
         {
             Assert.False(Program.IsLeapYear(1931));
+        }
+
+        [Fact]
+        public void Prints_yay()
+        {
+            // Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            // Act
+            Program.PrintIsLeapYear(1996);
+
+            // Assert
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Equal("yay", output);
+        }
+
+        [Fact]
+        public void Prints_nay()
+        {
+            // Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            // Act
+            Program.PrintIsLeapYear(1767);
+
+            // Assert
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Equal("nay", output);
         }
     }
 }
--- LeapYear/Program.cs
+++ LeapYear/Program.cs
@@ -6,23 +6,49 @@ namespace LeapYear
     {
         public static void Main(string[] args)
         {
+            GetIsLeapYear();
         }
 
-        public static bool IsLeapYear(int year) 
+        public static void GetIsLeapYear() 
+        {
+            int userInt = GetUserInput();
+            PrintIsLeapYear(userInt);
+        }
+
+        public static int GetUserInput()
+        {
+            var userInput = Console.ReadLine();
+            return Convert.ToInt32(userInput);
+        }
+
+        public static void PrintIsLeapYear(int year)
+        {
+            if (IsLeapYear(year))
+            {
+                Console.WriteLine("yay");
+            }
+            else
+            {
+                Console.WriteLine("nay");
+            }
+        }
+
+        public static bool IsLeapYear(int year)
         {
 
-            if (year % 4 == 0) 
+            if (year % 4 == 0)
             {
-                if (year % 100 == 0) 
+                if (year % 100 == 0)
                 {
                     return year % 400 == 0;
 
-                } else 
+                }
+                else
                 {
                     return true;
                 }
-                
-            } else 
+            }
+            else
             {
                 return false;
             }

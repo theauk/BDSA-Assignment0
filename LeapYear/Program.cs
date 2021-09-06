0dea99ef99c8f253aabc7e197fe9e2e501764a1e
Thea U. Kjeldsmark <49352969+theauk@users.noreply.github.com>
2021-09-06 16:54:01 +0200

    Tested and implemented IsLeapYear (#2)

---
+++ LeapYear/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeapYear
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        public static bool IsLeapYear(int year) 
+        {
+
+            if (year % 4 == 0) 
+            {
+                if (year % 100 == 0) 
+                {
+                    return year % 400 == 0;
+
+                } else 
+                {
+                    return true;
+                }
+                
+            } else 
+            {
+                return false;
+            }
+        }
+    }
+}
